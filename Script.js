000c63bb852597e23fcd7e2faf63966b4fe79cbf
NiketanG <nikegulekar@gmail.com>
2019-09-21 02:15:00 +0530

    Bugfixes

--- Script.js
+++ Script.js
@@ -2,7 +2,7 @@ window.onload = function load() {
     //Preloader 
     setTimeout(() => {
         $('.loader').fadeOut(500);
-    }, 1000);
+    }, 100);
 }
 
 function loadform() {
@@ -305,42 +305,36 @@ $(document).ready(function () {
     }, function () {
         $(".toggle").css("filter", "invert(1)");
     });
-
-
-
-
-    
 });
-
 
 
 // Set the date we're counting down to
 var countDownDate = new Date("Jan 1, 2020 08:30:00").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+var x = setInterval(function () {
 
-  // Get today's date and time
-  var now = new Date().getTime();
+    // Get today's date and time
+    var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var months = Math.floor(distance / (1000 * 60 * 60 * 24 * 30));
+    // Time calculations for days, hours, minutes and seconds
+    var months = Math.floor(distance / (1000 * 60 * 60 * 24 * 30));
 
-  var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 *24 ));
+    var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24));
 
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  $(".months h2").html(months);
-  $(".days h2").html(days);
-  $(".hours h2").html(hours);
+    $(".months h2").html(months);
+    $(".days h2").html(days);
+    $(".hours h2").html(hours);
 
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x); 
-  }
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+    }
 }, 1000);
