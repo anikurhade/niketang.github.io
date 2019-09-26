b98ad7e339d1ee0f4a608ce008df2c743eecd5d3
NiketanG <nikegulekar@gmail.com>
2019-09-26 20:58:51 +0530

    Bugfixes

--- Script.js
+++ Script.js
@@ -1,4 +1,5 @@
 window.onload = function load() {
+    this.set_counter();
     //Preloader 
     setTimeout(() => {
         $('.loader').fadeOut(500);
@@ -78,6 +79,29 @@ function toggle() {
     }
 }
 
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 1, 2020 09:30:00").getTime();
+// Get today's date and time
+var now = new Date().getTime();
+
+function set_counter() {
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+    // Time calculations for months, days, hours
+    var months = Math.floor(distance / (1000 * 60 * 60 * 24 * 30));
+    var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    $(".months h2").html(months);
+    $(".days h2").html(days);
+    $(".hours h2").html(hours);
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+    }
+}
+// Update the count down every 1 minute
+setInterval(set_counter, 1000);
+
 $(document).ready(function () {
     $("ul").hover(function () {
         $(".toggle").css("filter", "invert(0)");
@@ -85,3 +109,4 @@ $(document).ready(function () {
         $(".toggle").css("filter", "invert(1)");
     });
 });
+
